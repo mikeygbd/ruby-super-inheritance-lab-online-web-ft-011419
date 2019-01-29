3180091e867236b30b8648188b69c1eb934a727c
Michael Soares <mikey.soares@gmail.com>
2019-01-29 17:07:12 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,7 +8,7 @@ class ChattyStudent < Student
 
   def raise_hand
     super
-    puts "Pick me! \nPick me! \nPick me! \nPick me! \nPick me! \nPick me! \nPick me! \nPick me! \nPick me! \nPick me!"
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!"
 
 # 10.times{}
   end
